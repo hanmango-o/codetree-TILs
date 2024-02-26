fa7d7b0edc52979b1e70b26f212cec8fad08a503
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-02-26 14:20:48 +0900

    Title: 피보나치 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/피보나치 수/fibonacci-number.cpp
+++ 240226/피보나치 수/fibonacci-number.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int N;
 int dp[46];
+int memo[46];
+
+int Fibo(int n) {
+    if(n <= 2) return 1;
+    if(memo[n] > 0) return memo[n];
+    return Fibo(n - 1) + Fibo(n - 2);
+}
 
 int main() {
      dp[1] = 1;
