2bafa7c2e1afae0eaf89f5ce11a469334176ff42
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-02-26 14:25:27 +0900

    Title: 피보나치 수, Time: 1034ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240226/피보나치 수/fibonacci-number.cpp
+++ 240226/피보나치 수/fibonacci-number.cpp
@@ -13,12 +13,7 @@ int Fibo(int n) {
 }
 
 int main() {
-     dp[1] = 1;
-     dp[2] = 1;
      cin >> N;
-     for(int i = 3; i <= N; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
-     }
-     cout << dp[N];
+     cout << Fibo(N);
     return 0;
 }
