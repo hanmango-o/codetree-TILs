69e3c468bffe32943583e4336ce560876abfb38d
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-02-26 15:18:31 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/계단 오르기/climbing-stairs.cpp
+++ 240226/계단 오르기/climbing-stairs.cpp
@@ -2,22 +2,17 @@
 
 using namespace std;
 
-int memo[1001];
-
-int DP(int n) {
-    if(n < 0) return 0;
-    if(n == 2 || n == 3) return 1;
-    if(memo[n] != 0) return memo[n];
-    int a = DP(n - 2);
-    int b = DP(n - 3);
-    memo[n] += a + b;
-    return a + b;
-}
-
 int N;
+
+int dp[1001];
 
 int main() {
     cin >> N;
-    cout << DP(N) % 10007;
+    dp[0] = dp[2] = dp[3] = 1;
+    dp[1] = 0;
+    for(int i = 4; i <= N; i++) {
+        dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
+    }
+    cout << dp[N] % 10007;
     return 0;
 }
