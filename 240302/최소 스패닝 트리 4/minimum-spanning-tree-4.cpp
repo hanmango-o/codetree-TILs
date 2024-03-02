3bc6aeb066f9d9ce4aca8e8277e0752bba966946
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-03-02 11:38:50 +0900

    Title: 최소 스패닝 트리 4, Time: 118ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240302/최소 스패닝 트리 4/minimum-spanning-tree-4.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+int N, M, answer;
+char type[10001];
+int parent[10001];
+vector<tuple<int, int, int>> vect;
+
+int Find(int tar) {
+    if(tar == parent[tar]) return tar;
+    int ret = Find(parent[tar]);
+    parent[tar] = ret;
+    return ret;
+}
+
+void Union(int a, int b) {
+    int pa = Find(a);
+    int pb = Find(b);
+    if(pa == pb) return;
+    parent[pb] = pa;
+}
+
+int main() {
+    cin >> N >> M;
+    for(int i = 1; i <= N; i++) {
+        parent[i] = i;
+        cin >> type[i];
+    }
+    for(int i = 0; i < M; i++) {
+        int from, to, vol;
+        cin >> from >> to >> vol;
+        vect.push_back(make_tuple(vol, from ,to));
+    }
+    sort(vect.begin(), vect.end());
+    for(int i = 0; i < vect.size(); i++) {
+        int vol, from, to;
+        tie(vol, from, to) = vect[i];
+        if(Find(from) == Find(to)) continue;
+        if(type[from] == type[to]) continue;
+        Union(from, to);
+        answer += vol;
+    }
+    cout << (answer == 0 ? -1 : answer);
+    return 0;
+}
