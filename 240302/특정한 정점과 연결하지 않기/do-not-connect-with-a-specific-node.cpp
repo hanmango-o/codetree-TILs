a713c6aa2bda2dbab1488f0d426666f6116755ce
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-03-02 10:55:54 +0900

    Title: 특정한 정점과 연결하지 않기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240302/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.cpp
+++ 240302/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 
 int N, M, A, B, K, answer;
-int parent[100001];
+int parent[100001], DAT[100001];
+vector<pair<int, int>> vect;
 
 int Find(int tar) {
     if(tar == parent[tar]) return tar;
@@ -28,14 +32,26 @@ int main() {
         int from, to;
         cin >> from >> to;
         Union(from, to);
+        DAT[Find(from)]++;
+    }
+
+    for(int i = 1; i <= N; i++) {
+        if(DAT[i] > 0) {
+            vect.push_back(make_pair(DAT[i], i));
+        }
     }
     cin >> A >> B >> K;
     
-    for(int i = 1; i <= N; i++) {
-        if(Find(i) == Find(A)) continue;
-        if(Find(i) == Find(B)) continue;
+    sort(vect.begin(), vect.end(), greater<pair<int, int>>());
+
+    for(int i = 0; i < vect.size(); i++) {
+        int node, vol;
+        tie(node, vol) = vect[i];
+
+        if(Find(node) == Find(A)) continue;
+        if(Find(node) == Find(B)) continue;
         if(K > 0) {
-            Union(i, A);
+            Union(node, A);
             K--;
         }
     }
