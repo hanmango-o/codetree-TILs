ad30d15d4fd529519a0230ffa2bb2fec49a14f6f
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-03-02 10:41:18 +0900

    Title: 특정한 정점과 연결하지 않기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240302/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M, A, B, K, answer;
+int parent[100001];
+
+int Find(int tar) {
+    if(tar == parent[tar]) return tar;
+    int ret = Find(parent[tar]);
+    parent[tar] = ret;
+    return ret;
+}
+
+void Union(int a, int b) {
+    int pa = Find(a);
+    int pb = Find(b);
+    if(pa == pb) return;
+    parent[pb] = pa;
+}
+
+int main() {
+    cin >> N >> M;
+    for(int i = 1; i <= N; i++) {
+        parent[i] = i;
+    }
+    for(int i = 0; i < M; i++) {
+        int from, to;
+        cin >> from >> to;
+        Union(from, to);
+    }
+    cin >> A >> B >> K;
+    
+    for(int i = 1; i <= N; i++) {
+        if(Find(i) == Find(A)) {
+            answer++;
+            continue;
+        }
+        if(Find(i) == Find(B)) continue;
+        if(K--) {
+            Union(i, A);
+            answer++;
+        } 
+    }
+    cout << answer;
+    return 0;
+}
