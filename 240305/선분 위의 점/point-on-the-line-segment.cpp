7e863f9b4d80ca70b7d55263ae0ff65c1ca5aeda
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-03-05 11:22:44 +0900

    Title: 선분 위의 점, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240305/선분 위의 점/point-on-the-line-segment.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int arr[100000];
+
+int LowerBound(int left, int right, int target) {
+    int mid = 0, ret = N;
+    while(left <= right) {
+        mid = (left + right) / 2;
+        if(arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+            ret = min(ret, mid);
+        }
+    }
+    return ret;
+}
+
+int CustomBound(int left, int right, int target) {
+    int mid = 0, ret = -1;
+    while(left <= right) {
+        mid = (left + right) / 2;
+        if(arr[mid] <= target) {
+            left = mid + 1;
+            ret = max(ret, mid);
+        } else {
+            right = mid - 1;
+        }
+    }
+    return ret;
+}
+
+int main() {
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    for(int i = 0; i < M; i++) {
+        int x, y;
+        cin >> x >> y;
+        int tempX = LowerBound(0, N - 1, x);
+        int tempY = CustomBound(0, N - 1, y);
+        if(arr[tempX] != x) tempX--;
+        cout << tempY - tempX << "\n";
+    }
+    return 0;
+}
