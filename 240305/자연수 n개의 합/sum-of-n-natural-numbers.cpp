76c76fdc62af672be166be2c5550470b89723f8e
한망고 <88955964+hanmango-o@users.noreply.github.com>
2024-03-05 13:14:11 +0900

    Title: 자연수 n개의 합, Time: 1071ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240305/자연수 n개의 합/sum-of-n-natural-numbers.cpp
+++ 240305/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 long long S;
+unordered_map<long long, long long> um;
 
-long long getSum(long long n) {
-    long long sum = 0;
-    for(long long i = 1; i <= n; i++) {
-        sum += i;
+long long getSum(long long n, long long before) {
+    long long sum = 0, temp = 0;
+    if(um.find(before)!=um.end()) temp = um[before];
+    if(n < before) {
+        for(long long i = n + 1; i <= before; i++) {
+            sum -= i;
+        }
+    } else {
+        for(long long i = before + 1; i <= n; i++) {
+            sum += i;
+        }
     }
-    return sum;
+    
+    return sum + temp;
 }
 
 long long BS(long long left, long long right) {
     long long mid = 0;
+    long long before = 0;
     while(left <= right) {
         mid = (left + right) / 2;
-        int sum = getSum(mid);
+        long long sum = getSum(mid, before);
         if(sum == S) return mid;
         else if(sum < S) left = mid + 1;
         else right = mid - 1;
+        um.insert({mid, sum});
+        before = mid;
     }
     return mid - 1;
 }
